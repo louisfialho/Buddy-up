5ac3f0fd78192361b8d10d203c88e0c563a37046
Louis Fialho <louis.fialho@hec.edu>
2020-09-01 16:29:52 +0100

    location search

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,20 @@
 class EventsController < ApplicationController
   def index
     @events = Event.all
+
+    if search_params.present?
+      @location = search_params["location"]
+      @events = Event.where("location ILIKE ?", @location)
+    end
   end
 
   def show
   end
+
+ private
+
+  def search_params
+    params.require(:search).permit(:location)
+  end
+
 end
