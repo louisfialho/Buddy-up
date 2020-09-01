bc6919e02f9a45703261e040523a6cecc12416a1
Christopher Nitsche <christopher.nitsche@gmx.de>
2020-09-01 15:06:51 +0100

    Stil need to adjust start and end time at some point of time

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_31_164857) do
+ActiveRecord::Schema.define(version: 2020_09_01_134857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,6 @@ ActiveRecord::Schema.define(version: 2020_08_31_164857) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "sport"
-    t.date "date"
     t.time "start_time"
     t.time "end_time"
     t.string "location"
