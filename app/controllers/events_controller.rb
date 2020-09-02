9986e10b13a64f80128a73c50c4a77af951ea6b8
Ricardo Araujo <ricardoluisaraujo2006@gmail.com>
2020-09-02 15:23:07 +0100

    map almost done. Only some details in the pop-up and it's over

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -31,7 +31,9 @@ class EventsController < ApplicationController
     @markers = @events.geocoded.map do |event|
       {
         lat: event.latitude,
-        lng: event.longitude
+        lng: event.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { event: event })
+
       }
     end
   end
