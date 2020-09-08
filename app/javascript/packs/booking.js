b0c6d81ee3935133b34d753a20a317b6c653116b
Louis Fialho <louis.fialho@hec.edu>
2020-09-08 17:01:30 +0200

    decrement working

--- app/javascript/packs/booking.js
+++ app/javascript/packs/booking.js
@@ -1,25 +1,32 @@
 export function add_participants() {
 
-const button = document.getElementById('plus')
-button.addEventListener('click', (event) => {
-  console.log("+")
-  // selecting the counter
-  const counter = document.getElementById('counter');
-  // defining offset which is either 1 or -1
-  const offset = Number.parseInt(event.currentTarget.dataset.offset, 10);
-  // defining count which should be the new value of the counter (current value + offset)
-  const count = Number.parseInt(counter.dataset.count, 10) + offset;
-  console.log(count)
-  // replacing
+const updateCounter = (counter, count) => {
   counter.dataset.count = count;
-  console.log(count)
-  counter.innerHTML = count;
-  console.log(count)
-});
-
-
-
+  counter.innerText = count;
 };
+
+
+  const updateForm = (event) => {
+  const offset = Number.parseInt(event.currentTarget.dataset.offset, 10);
+  const counter = document.getElementById('counter');
+  const count = Number.parseInt(counter.dataset.count, 10) + offset;
+  if (count >= 1) {
+    updateCounter(counter, count);
+  } else {
+    event.preventDefault();
+  }
+};
+
+
+  const updateFormOnButtonClick = (button) => {
+  button.addEventListener('click', updateForm);
+};
+
+  const buttons = document.querySelectorAll('.incrementer');
+  buttons.forEach(updateFormOnButtonClick);
+};
+
+
 
 
 
