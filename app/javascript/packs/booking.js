96d8b0a558a295e6c6a85a7355e6c4d7678d2d83
Louis Fialho <louis.fialho@hec.edu>
2020-09-08 16:27:21 +0200

    increment

--- app/javascript/packs/booking.js
+++ app/javascript/packs/booking.js
@@ -2,11 +2,19 @@ export function add_participants() {
 
 const button = document.getElementById('plus')
 button.addEventListener('click', (event) => {
- // const offset = Number.parseInt(event.currentTarget.dataset.offset, 10);
- // const counter = document.getElementById('counter');
- // const count = Number.parseInt(counter.dataset.count, 10) + offset;
- // counter.dataset.count = 100;
- counter.innerText = 100;
+  console.log("+")
+  // selecting the counter
+  const counter = document.getElementById('counter');
+  // defining offset which is either 1 or -1
+  const offset = Number.parseInt(event.currentTarget.dataset.offset, 10);
+  // defining count which should be the new value of the counter (current value + offset)
+  const count = Number.parseInt(counter.dataset.count, 10) + offset;
+  console.log(count)
+  // replacing
+  counter.dataset.count = count;
+  console.log(count)
+  counter.innerHTML = count;
+  console.log(count)
 });
 
 
